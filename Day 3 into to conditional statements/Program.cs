0d15904b568139e6856f929fc06c7e68a3bde847
Safura234 <safuramg@code.edu.az>
2022-05-18 12:08:41 +0400

    https://github.com/Safura234/-ntro-to-conditional-statements-Hacker.git

---
+++ Day 3 into to conditional statements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Day_3_into_to_conditional_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+            if (N % 2 == 1)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (N % 2 == 0 && N > 2 && N < 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (N % 2 == 0 && N > 6 && N <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (N % 2 == 0 && N > 20)
+            {
+                Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
